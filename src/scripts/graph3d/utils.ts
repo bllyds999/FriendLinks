373d319028b1330fbfef4b4948624335b44bf8ce
王兴家 <xingwangzhe@outlook.com>
2026-07-01 17:52:55 +0800

    fix: 标签 h=10 (原始5的2x)
    Signed-off-by: 王兴家 <xingwangzhe@outlook.com>

--- src/scripts/graph3d/utils.ts
+++ src/scripts/graph3d/utils.ts
@@ -182,7 +182,7 @@ export function createTextSprite(text: string): THREE.Sprite {
   });
 
   const sprite = new THREE.Sprite(mat);
-  const h = 17; // world-unit height
+  const h = 10; // world-unit height
   sprite.scale.set(h * (canvas.width / canvas.height), h, 1);
   sprite.renderOrder = 999;
 
